52dbb21135324fbc257f596452d8b720e69503c3
markhuge <mark@markwilkerson.me>
2016-09-02 22:37:08 -0700

    wait does this work now?

--- gulpfile.js
+++ gulpfile.js
@@ -15,26 +15,26 @@ gulp.task('css', () => {
   gulp.src(paths.sass)
    .pipe(sass().on('error', sass.logError))
    .pipe(prefix())
-   .pipe(gulp.dest('./dist/'))
+   .pipe(gulp.dest('./docs/'))
    .pipe(sync.stream())
 })
 
 
 gulp.task('html', () => {
   gulp.src(paths.html)
-    .pipe(gulp.dest('./dist/'))
+    .pipe(gulp.dest('./docs/'))
     .pipe(sync.stream())
 })
 
 gulp.task('images', () => {
   gulp.src(paths.images)
-    .pipe(gulp.dest('./dist/'))
+    .pipe(gulp.dest('./docs/'))
     .pipe(sync.stream())
 })
 
 gulp.task('sync', () => {
   sync.init({
-    server: { baseDir: './dist' }
+    server: { baseDir: './docs' }
   })
   gulp.watch(paths.sass,['css']).on('change', sync.reload)
   gulp.watch(paths.html,['html']).on('change', sync.reload)
